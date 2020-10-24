fc0ae0c973f57dc9d9ebe79b48f40eaf4256600d
Dylan Perales <dperales@gmail.com>
2020-10-23 20:32:58 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TimeAdder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] timeAdder(int value1, string label1, int value2, string label2)
+            {
+
+            }
+        }
+    }
+}
